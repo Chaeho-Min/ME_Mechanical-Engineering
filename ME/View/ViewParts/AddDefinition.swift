618506dc83ff0e9028010e9c47f78e4a3cf180d6
Chaeho-Min <james118@naver.com>
2022-06-03 08:53:47 +0900

    Add CoreData Model and DataController

--- ME/View/ViewParts/AddDefinition.swift
+++ ME/View/ViewParts/AddDefinition.swift
@@ -11,6 +11,7 @@ struct AddDefinition: View {
     @Environment(\.dismiss) var dismiss
     @State private var title: String = ""
     @State private var description: String = ""
+    @State private var mechanicTitle: String = mechanicsData[0].title
     
     var body: some View {
         ZStack {
@@ -20,7 +21,24 @@ struct AddDefinition: View {
                 navigationBar
                 VStack {
                     TextField("Title", text: $title)
+                        .font(.headline)
+                        .disableAutocorrection(true)
                     TextField("Description", text: $description)
+                        .disableAutocorrection(true)
+                    Divider()
+                    HStack {
+                        Text("Mechanics")
+                            .font(.headline)
+                        Spacer()
+                        Picker("Choose Mechanics", selection: $mechanicTitle) {
+                            ForEach(mechanicsData) { mechanics in
+                                HStack {
+                                    Image(systemName: mechanics.systemName)
+                                    Text(mechanics.title)
+                                }
+                            }
+                        }
+                    }
                 }
                 .groupedBG(cornerRadius: 12, color: .secondaryBG)
                 .padding(.vertical)
